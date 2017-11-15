c29be91c6e946ad3468ea73d0287047cbbe069d3
theimberger <theimberger@gmail.com>
2017-11-15 17:28:47 -0500

    Add Todo list functionality

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
 s4(() => {
-  var close = s4('.close');
 
-  // close.on("click", (e) => {
-  //   console.log(e.currentTarget);
-  //   debugger
-  //   s4(e.currentTarget);
-  //   // console.log(target.parent());
-  //
-  // });
+  setTodos();
+  var addTodo = s4(".add_todo")
+  var addButton = s4('.add_button');
 
+  addTodo.on("keydown", createTodo);
+
+  addButton.on("click", createTodo);
 });
+
+const setTodos = () => {
+  var close = s4('.close');
+  close.on("click", (e) => {
+    s4(e.currentTarget).parent().remove();
+  });
+};
+
+const createTodo = (e) => {
+  let input = s4('.add_todo');
+  if (e.type === "click" || e.keyCode === 13) {
+    s4('.todo_list').append(
+      `<li class="todo">
+        <span>${input.attr("value")}</span>
+        <span class="close">✕</span>
+      </li>`
+    );
+    input.arr[0].value = ("");
+    setTodos();
+  }
+};
