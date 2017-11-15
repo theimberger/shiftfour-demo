e760b7fe80def0de05fe8f8a2b4026d16769c3ed
theimberger <theimberger@gmail.com>
2017-11-15 15:46:31 -0500

    Add todo widget

---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+This is a demo page for the shiftFour library, found [here](https://github.com/theimberger/shiftfour).
+To see the live demo, look [here](https://theimberger.github.io/shiftfour_demo/).
---
+++ main.js
@@ -0,0 +1,12 @@
+s4(() => {
+  var close = s4('.close');
+
+  // close.on("click", (e) => {
+  //   console.log(e.currentTarget);
+  //   debugger
+  //   s4(e.currentTarget);
+  //   // console.log(target.parent());
+  //
+  // });
+
+});
