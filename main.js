919a31a805863b59765fdf919e85c79243625fe1
theimberger <theimberger@gmail.com>
2017-11-16 17:38:53 -0500

    Add weather widget

--- main.js
+++ main.js
@@ -32,6 +32,14 @@ s4(() => {
     cells.children().forEach( el => changeColor(el));
   });
 
+  //code for youtube fetcher
+  var weatherSearch = s4('.weather_search');
+  var weatherGo = s4('.weather_go');
+
+  weatherSearch.on("keydown", requestWeather);
+  weatherGo.on('click', requestWeather);
+
+
 });
 
 const setTodos = () => {
@@ -83,3 +91,54 @@ const changeColor = (element) => {
   color = "#" + color;
   s4(element).css("background-color", color);
 };
+
+const requestWeather = (e) => {
+  if (e.type === "keydown" && e.keyCode !== 13) {
+    return;
+  }
+  console.log("fired");
+
+  let query = s4('.weather_search').attr("value")[0];
+
+  const gotWeather = (data) => {
+    if (data.target.status !== 200) {
+      weatherFail();
+      return;
+    }
+
+    let weather = JSON.parse(data.target.response);
+    weather.main = weather.main.temp;
+    weather.main = Math.floor(weather.main * 9/5 - 459.67);
+    weather.weather = weather.weather[0].main;
+    console.log(weather);
+    s4(".weather_info").html(
+      `The current temperature/weather in ${query} is ${weather.main}°F / ${weather.weather}.`
+    );
+  };
+
+  const weatherFail = () => {
+    s4(".weather_info").html(
+      `Couldn't get the weather for ${query}.`
+    );
+  }
+
+  s4.ajax({
+    method: "GET",
+    url: `http://api.openweathermap.org/data/2.5/weather?q=${query}&APPID=b7a8f75ded6b46eb224c6a0ef234d186`,
+    success: gotWeather,
+    failure: weatherFail
+  });
+
+};
+
+
+//
+//
+//
+//
+// 'GET',
+//                 '/youtube/v3/search',
+//                 {'maxResults': '25',
+//                  'part': 'snippet',
+//                  'q': 'surfing',
+//                  'type': ''});
