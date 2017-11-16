7613282d98fa821f4ad795f0e378110dde59c829
theimberger <theimberger@gmail.com>
2017-11-16 12:22:25 -0500

    Add color grid widget

--- ReadMe.md
+++ ReadMe.md
@@ -1,2 +1,4 @@
-This is a demo page for the shiftFour library, found [here](https://github.com/theimberger/shiftfour).
-To see the live demo, look [here](https://theimberger.github.io/shiftfour_demo/).
+# shiftFour Demo Page
+This is a simple demo page for the shiftFour library, which is a lightweight javascript library you can find [here](https://github.com/theimberger/shiftfour).
+
+To see the demo page itself, look [here](https://theimberger.github.io/shiftfour_demo/).
--- main.js
+++ main.js
@@ -1,12 +1,37 @@
 s4(() => {
 
+  //code for todo widget
+
   setTodos();
   var addTodo = s4(".add_todo")
   var addButton = s4('.add_button');
 
   addTodo.on("keydown", createTodo);
-
   addButton.on("click", createTodo);
+
+  //code for the color grid widget
+
+  createTable();
+  var cells = s4('td');
+  var colorReset = s4('#color_reset');
+  var colorFill = s4('#color_fill');
+
+  cells.on("mouseenter", (e) =>
+    changeColor(e.currentTarget)
+  );
+
+  colorReset.on('click', () => {
+    cells.css("background-color", "#ffffff");
+  });
+
+  colorReset.on('click', () => {
+    cells.css("background-color", "#ffffff");
+  });
+
+  colorFill.on('click', () => {
+    cells.children().forEach( el => changeColor(el));
+  });
+
 });
 
 const setTodos = () => {
@@ -28,4 +53,33 @@ const createTodo = (e) => {
     input.arr[0].value = ("");
     setTodos();
   }
+};
+
+const createTable = () => {
+  let table = s4('tbody');
+  let i = 0;
+  let tr = "<tr></tr>";
+  let td = "<td></td>";
+  let current;
+
+  while (i < 20) {
+    table.append(tr);
+    current = s4('tr').last();
+    let j = 0;
+    while (j < 20) {
+      current.append(td);
+      j ++;
+    }
+    i ++;
+  }
+};
+
+const changeColor = (element) => {
+  let color = "";
+  let characters = "0123456789abcdef";
+  for (let i = 0; i < 6; i++){
+    color += characters[Math.floor(Math.random() * 16)];
+  }
+  color = "#" + color;
+  s4(element).css("background-color", color);
 };
